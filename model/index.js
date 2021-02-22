dbcc8b3297e5c033e50ceb0facb9e1c08e8d83e1
Alexander Kholodnyak <kholodnyak.a@gmail.com>
2021-02-22 04:26:07 +0200

    start

--- model/index.js
+++ model/index.js
@@ -1,13 +1,48 @@
-// const fs = require('fs/promises')
-// const contacts = require('./contacts.json')
+const fs = require('fs/promises')
+// const contacts = require('./contacts.json');
+const path = require('path');
+const { v4: uuid } = require('uuid')
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname,'./contacts.json');
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+  data = await fs.readFile(contactsPath, "utf-8");
+  return JSON.parse(data);
+    
+}
 
-const removeContact = async (contactId) => {}
+const getContactById = async (contactId) => {
+    data = await fs.readFile(contactsPath, "utf-8");
+        
+    const contacts = JSON.parse(data);
+    const contactItem = contacts.find((contact) => String(contact.id) === contactId);
+        
+    return contactItem;
+        
+}
 
-const addContact = async (body) => {}
+const removeContact = async (contactId) => {
+  data = await fs.readFile(contactsPath, "utf-8");
+        
+  const contacts = JSON.parse(data);
+  console.log(contacts);
+  const contactItem = contacts.filter((contact) => String(contact.id) !== contactId);
+  console.log(String(contactId));
+  await fs.writeFile(contactsPath, JSON.stringify(contactItem));
+  return contactItem;
+ 
+    
+}
+
+const addContact = async (body) => {
+  const id = uuid();
+  const data =await fs.readFile(contactsPath, "utf-8");
+  const contacts = JSON.parse(data);
+  const newContact = {id, ...body};
+  const newContacts = [...contacts, newContact];
+  await fs.writeFile(contactsPath, JSON.stringify(newContacts));
+  return newContact;
+};
 
 const updateContact = async (contactId, body) => {}
 
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,24 +1,114 @@
 const express = require('express')
 const router = express.Router()
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} = require('../../model/index.js')
+
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const contacts = await listContacts();
+    return res.json({
+      status: 'success',
+      code: 200,
+      data: {
+        contacts,
+      },
+    })
+  } catch (e) {
+    next(e)
+  }
 })
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+router.get('/:id', async (req, res, next) => {
+  try {
+    const contact = await getContactById(req.params.id)
+    if (contact) {
+      return res.json({
+        status: 'success',
+        code: 200,
+        data: {
+          contact,
+        },
+      })
+    } else {
+      return res.status(404).json({
+        status: 'error',
+        code: 404,
+        data: 'Not Found',
+      })
+    }
+  } catch (e) {
+    next(e)
+  }
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+   try {
+    const cat = await addContact(req.body)
+    return res.status(201).json({
+      status: 'success',
+      code: 201,
+      data: {
+        cat,
+      },
+    })
+  } catch (e) {
+    next(e)
+  }
 })
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const contact = await removeContact(req.params.id)
+    if (contact) {
+      return res.json({
+        status: 'success',
+        code: 200,
+        data: {
+          contact,
+        },
+      })
+    } else {
+      return res.status(404).json({
+        status: 'error',
+        code: 404,
+        data: 'Not Found',
+      })
+    }
+  } catch (e) {
+    next(e)
+  }
 })
 
 router.patch('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  // '/:id/vaccinated',
+  // validate.updateStatusCat,
+  // async (req, res, next) => {
+  //   try {
+  //     const cat = await Cats.update(req.params.id, req.body)
+  //     if (cat) {
+  //       return res.json({
+  //         status: 'success',
+  //         code: 200,
+  //         data: {
+  //           cat,
+  //         },
+  //       })
+  //     } else {
+  //       return res.status(404).json({
+  //         status: 'error',
+  //         code: 404,
+  //         data: 'Not Found',
+  //       })
+  //     }
+  //   } catch (e) {
+  //     next(e)
+  //   }
+  // },
 })
 
 module.exports = router
